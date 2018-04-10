29ccec21879ca878bc33cc739173047b97d40285
Bill Dybas <wmdybas@gmail.com>
2018-04-10 19:48:37 -0400

    Add react-select Components

---
+++ src/react-select/FormikCreatable.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Creatable } from 'react-select'
+
+class FormikCreatable extends Component {
+  static propTypes = {
+    field: PropTypes.string.isRequired,
+    input: PropTypes.shape({
+      value: PropTypes.oneOfType([
+        PropTypes.shape({
+          value: PropTypes.string,
+          label: PropTypes.string
+        }),
+        PropTypes.arrayOf(
+          // If 'multi' select
+          PropTypes.shape({
+            value: PropTypes.string,
+            label: PropTypes.string
+          })
+        ),
+        PropTypes.string // '' when empty
+      ]),
+      onChange: PropTypes.func.isRequired, // setFieldValue
+      onBlur: PropTypes.func.isRequired // setFieldTouched
+    }).isRequired
+  }
+
+  handleChange = value => {
+    this.props.input.onChange(this.props.field, value)
+  }
+
+  handleBlur = () => {
+    this.props.input.onBlur(this.props.field, true)
+  }
+
+  render () {
+    const { input, field, ...rest } = this.props
+
+    return (
+      <Creatable
+        {...rest}
+        // Required props
+        onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        value={input.value}
+      />
+    )
+  }
+}
+
+export default FormikCreatable
---
+++ src/react-select/README.md
@@ -0,0 +1,12 @@
+# [react-select](https://github.com/JedWatson/react-select)
+
+Don't forget to add styles:
+
+```js
+import 'react-select/dist/react-select.css'
+```
+
+## Supported Components
+
+- Creatable
+- Select
